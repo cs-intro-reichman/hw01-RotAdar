e8d49e44bb0322cf71129607db9b3d2837e75759
Rotem Adar <154267575+RotAdar@users.noreply.github.com>
2023-12-24 22:39:43 +0200

    Update GenThree.java

--- GenThree.java
+++ GenThree.java
@@ -3,12 +3,11 @@ public static void main(String[] args) {
 {
 int min= Integer.parseInt(args[0]);
 int max= Integer.parseInt(args[1]);
-int rand1= (int)((Math.random())*(max-min))+min;
-int rand2= (int)((Math.random())*(max-min))+min;
-int rand3= (int)((Math.random())*(max-min))+min;
-int randMin=Math.min((Math.min(rand1,rand2)),rand3);
+int rand1= (int)((Math.random()) * (max - min)) + min;
+int rand2= (int)((Math.random()) * (max - min)) + min;
+int rand3= (int)((Math.random()) * (max - min)) + min;
+int randMin= Math.min((Math.min(rand1, rand2)), rand3);
 System.out.println( rand1 + "\n" + rand2 + "\n" + rand3 + "\n" + "The minimal generated number was " + randMin);
-
 }
 }
 }
