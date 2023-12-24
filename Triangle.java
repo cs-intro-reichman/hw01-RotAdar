e43684ad60f4879e2fda6ec2f42311f2681fff06
Rotem Adar <154267575+RotAdar@users.noreply.github.com>
2023-12-24 22:36:50 +0200

    Update Triangle.java

--- Triangle.java
+++ Triangle.java
@@ -4,13 +4,10 @@ public static void main(String[] args) {
 int a=Integer.parseInt(args[0]);
 int b=Integer.parseInt(args[1]);
 int c=Integer.parseInt(args[2]);
-if (((a+b)>c)&&((b+c)>a)&&((a+c)>b))
-{
-    System.out.println(a+ ", "+ b + ", "+ c+ ": true");
-}
-else
-{
-    System.out.println(a+ ", "+ b + ", "+ c+ ": false");
+if (((a + b) > c) && ((b + c) > a) && ((a + c) > b)) {
+    System.out.println(a + ", "+ b + ", " + c + ": true");
+} else {
+    System.out.println(a + ", " + b + ", " + c + ": false");
 }
 }
 }
