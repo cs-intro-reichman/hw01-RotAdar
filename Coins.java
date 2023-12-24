0ccc84c5121c1d4beb25e5652b08cd2ce9fa299e
Rotem Adar <154267575+RotAdar@users.noreply.github.com>
2023-12-24 22:52:42 +0200

    Update Coins.java

--- Coins.java
+++ Coins.java
@@ -1,11 +1,15 @@
+/*
+ * Write a program that gets a quantity of cents as a command-line argument.
+ * The program prints how to represent this quantity using as many quarters as possible, plus the remainder in cents.
+ */
 public class Coins {
 public static void main(String[] args) {
 {
-
+  //convert to int the input from user and than check how many quater and cent i need to use
 int quarter= Integer.parseInt(args[0]);
 int cents= quarter % 25;
 quarter= ((quarter - cents) / 25);
-System.out.println("Use " + quarter + " quarters and " + cents + " cents");
+System.out.println("Use " + quarter + " quarters and " + cents + " cents");//printing how many quaters and cents we can use that we use minimally ammount of cents th
 }
 }
 }
