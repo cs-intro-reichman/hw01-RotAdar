5c6ba66d12d99cabcb61ce286ddefacb03aacdd2
Rotem Adar <154267575+RotAdar@users.noreply.github.com>
2023-12-24 22:37:47 +0200

    Update Coins.java

--- Coins.java
+++ Coins.java
@@ -3,8 +3,8 @@ public static void main(String[] args) {
 {
 
 int quarter= Integer.parseInt(args[0]);
-int cents= quarter%25;
-quarter= ((quarter-cents) / 25);
+int cents= quarter % 25;
+quarter= ((quarter - cents) / 25);
 System.out.println("Use " + quarter + " quarters and " + cents + " cents");
 }
 }
