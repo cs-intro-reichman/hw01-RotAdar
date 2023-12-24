f3060596966b39f8a22cbbf7f72afd4771940ff1
Rotem Adar <154267575+RotAdar@users.noreply.github.com>
2023-12-24 23:05:29 +0200

    Update LinearEq.java

--- LinearEq.java
+++ LinearEq.java
@@ -2,15 +2,15 @@
  * Solves linear equations of the form a⋅x + b = c.
  * The program gets a, b, and c as command-line arguments,
  * computes x, and prints the result.
- * Treats the three arguments as well as the computed value as double values
+ * Treats the three arguments as well as the computed value as double values.
  */
 public class LinearEq {
 public static void main(String[] args) {
 {
 
-double a=Double.parseDouble(args[0]);
-double b=Double.parseDouble(args[1]);
-double c=Double.parseDouble(args[2]);
+double a= Double.parseDouble(args[0]);
+double b= Double.parseDouble(args[1]);
+double c= Double.parseDouble(args[2]);
 System.out.println(a + " * x + " + b + " = " + c);
 System.out.println("x = " + ((c - b) / a));
 
