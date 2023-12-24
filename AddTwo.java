c66406f1a851afbc3fcc9905cd26aa2b7f29b06d
Rotem Adar <154267575+RotAdar@users.noreply.github.com>
2023-12-24 22:33:26 +0200

    Update AddTwo.java

--- AddTwo.java
+++ AddTwo.java
@@ -3,7 +3,7 @@ public static void main(String[] args) {
 {
 int a= Integer.parseInt(args[0]);
 int b= Integer.parseInt(args[1]);
-System.out.println(a + " + " + b + " = " + (a+b));
+System.out.println(a + " + " + b + " = " + (a + b));
 }
 }
 }
