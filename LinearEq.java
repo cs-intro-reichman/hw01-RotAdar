517214f1473152602d084f7a052cba1d378cf3fc
Rotem Adar <154267575+RotAdar@users.noreply.github.com>
2023-12-24 22:32:59 +0200

    Update LinearEq.java

--- LinearEq.java
+++ LinearEq.java
@@ -6,7 +6,7 @@ double a=Double.parseDouble(args[0]);
 double b=Double.parseDouble(args[1]);
 double c=Double.parseDouble(args[2]);
 System.out.println(a + " * x + " + b + " = " + c);
-System.out.println("x = " + ((c-b)/a));
+System.out.println("x = " + ((c - b) / a));
 
 }
 }
